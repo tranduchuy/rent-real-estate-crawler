30bfa8ef12bc1c92c1a665b43c184cabf64166a2
longnguyen <nguyenbinhlong92@gmail.com>
2018-10-06 10:55:10 +0700

    rabbit mq test

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -27,6 +27,8 @@ const postSale = function (par, ch, conn) {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
             'accesstoken': API.tokenUser,
         },
+        rejectUnauthorized: false,
+        strictSSL: false
     };
     try {
         request(option, (err, httpResponse, body) => {
