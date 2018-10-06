145370afe89283d9640a8e1a6ab20d56aaa8d104
longnguyen <nguyenbinhlong92@gmail.com>
2018-10-06 10:41:05 +0700

    rabbit mq test

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -23,7 +23,6 @@ const postSale = function (par, ch, conn) {
         json: params,
         method: 'POST',
         headers: {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
             'accesstoken': API.tokenUser,
         },
     };
