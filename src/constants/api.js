f477c5c44ebef2c0725067c3fddc762343dc27d8
Long Nguyen <nguyenbinhlong92@gmail.com>
2019-03-13 09:55:30 +0700

    update access token for user Admin CDP

--- src/constants/api.js
+++ src/constants/api.js
@@ -3,7 +3,7 @@ const API_HOST = 'https://api.hecta.vn/';
 global.API = {
     captchaToken: 'captchaToken',
     tokenUser: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im5ndXllbmJpbmhsb25nOTJAZ21haWwuY29tIiwiaWF0IjoxNTUwMzA0ODEwLCJleHAiOjE1NTA0Nzc2MTB9.HvHv3MuH3T4Geym3fPUHRO_fVqocUUSUjoYjaiRUSx4',
-    tokenAdmin: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1hc3RlckBnbWFpbC5jb20iLCJpYXQiOjE1NTAzMDU3NjYsImV4cCI6MTU1MDQ3ODU2Nn0.j72m3v2ax5F_kInO_e4Ixp9UMA-iu_vXmfd6NzJAEaE',
+    tokenAdmin: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1hc3RlckBnbWFpbC5jb20iLCJpYXQiOjE1NTI0NDA3ODcsImV4cCI6MTU1MjYxMzU4N30.49XHvEP9peSKjePvguuQ8yT6DVVHryAjj78Gea7fzJo',
     postSale: API_HOST + 'api/v1/sales/add',
     postBuy: API_HOST + 'api/v1/buys/add',
     postNews: API_HOST + 'admin/v1/news/add',
