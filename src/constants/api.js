d41ba8401d9f129818f34d502389f43f6b7adc8f
Long Nguyen <nguyenbinhlong92@gmail.com>
2019-05-02 21:28:49 +0700

    update config crawler to debug

--- src/constants/api.js
+++ src/constants/api.js
@@ -3,7 +3,7 @@ const API_HOST = 'https://api.hecta.vn/';
 global.API = {
     captchaToken: 'captchaToken',
     tokenUser: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im5ndXllbmJpbmhsb25nOTJAZ21haWwuY29tIiwiaWF0IjoxNTUwMzA0ODEwLCJleHAiOjE1NTA0Nzc2MTB9.HvHv3MuH3T4Geym3fPUHRO_fVqocUUSUjoYjaiRUSx4',
-    tokenAdmin: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFwcG5ldHdhbWFuYWdlckBnbWFpbC5jb20iLCJpYXQiOjE1NTI1NjgwNDIsImV4cCI6MTU1Mjc0MDg0Mn0.qAIkpSfxWprtW_jgcWnUb6fKbMlFpkdj5R0zTELeQ3A',
+    tokenAdmin: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFwcG5ldHdhbWFuYWdlckBnbWFpbC5jb20iLCJpYXQiOjE1NTY4MDU2MDcsImV4cCI6MTU1Njk3ODQwN30.VBlJYjSgaflbIgtdKVo2Bibp6861j8DWYERHgGhNigQ',
     postSale: API_HOST + 'api/v1/sales/add',
     postBuy: API_HOST + 'api/v1/buys/add',
     postNews: API_HOST + 'admin/v1/news/add',
